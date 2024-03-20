751d6155efca2310c10f70125173096b711b0da0
dhack01 <don@donhackman.com>
2024-03-20 08:44:20 -0400

    made a react app
    still WIP

---
+++ hh/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <p>Hackman Handmades</p>
+            <br/>
+              <a mailto="contact@hackmanhandmades.com" style={{ textDecoration: 'none', color: 'darkorange' }} >contact@</a>
+            <br></br>
+            <br></br>
+              <a href="https://www.facebook.com/hackmanhandmades/" style={{ textDecoration: 'none' }} 
+              target="_blank" 
+              rel="noopener noreferrer">
+                <img src="f_logo_RGB-White_58.png" alt="fb" style={{width: '30px', height: '30px'}} />
+              </a>
+            
+              <a href="https://www.instagram.com/hackmanhandmades/" style={{ textDecoration: 'none' }} 
+              target="_blank" 
+              rel="noopener noreferrer">
+                <img src="insta100.png" alt="insta" style={{width: '30px', height: '30px'}} />
+              </a>
+            </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
